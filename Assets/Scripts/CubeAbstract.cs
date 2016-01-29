3cf5cb2753647bd2413f9f987baf91c8d3bd69dc
raimonds <raimis0001@gmail.com>
2016-01-30 00:51:56 +0200

    Player prefab

---
+++ Assets/Scripts/CubeAbstract.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeAbstract 
+{
+
+
+	//public abstract void OnPlayerLanded();
+
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class PlayerControler : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Collision enetered:" + collision.gameObject.name);
+	}
+}
