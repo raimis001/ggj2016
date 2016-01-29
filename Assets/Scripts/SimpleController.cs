e7b20f47a2685f7c023ec5e2ca0da2f129046348
raimonds <raimis0001@gmail.com>
2016-01-30 00:24:20 +0200

    Character moving

--- Assets/Scripts/SimpleController.cs
+++ Assets/Scripts/SimpleController.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class SimpleController : MonoBehaviour
 {
+
+	public GameObject Player;
+
     Mountain Mountain;
     /// <summary>
     /// Input direction
     /// </summary>
     float Direction;
+	Rigidbody playerBody;
+	Animator playerAnimator;
+	bool moving = false;
 
     Cube ActiveCube
     {
@@ -39,6 +45,32 @@ public class SimpleController : MonoBehaviour
     {
         Mountain = GameObject.FindGameObjectWithTag("Mountain").GetComponent<Mountain>();
         ActiveCube = Mountain.Root;
+		Player.transform.position = ActiveCube.transform.position + new Vector3(-0.5f,0.5f,-0.5f);
+		playerBody = Player.GetComponent<Rigidbody>();
+		playerAnimator = Player.GetComponent<Animator>();
+  }
+
+	IEnumerator MoveChar(float direction)
+	{
+		if (moving)
+		{
+			yield break;
+		}
+
+		moving = true;
+
+		Vector3 startPos = Player.transform.position;
+		Vector3 endPos = startPos + new Vector3(direction < 0 ? 1 : 0, 0, direction > 0 ? 1 : 0);
+		float dTime = 0.5f;
+		float tmp = 0;
+		while (tmp < dTime)
+		{
+			Player.transform.position = Vector3.Lerp(startPos, endPos, tmp / dTime);
+			tmp += Time.smoothDeltaTime;
+			yield return null;
+		}
+
+		moving = false;
 	}
 	
 	// Update is called once per frame
@@ -48,6 +80,9 @@ public class SimpleController : MonoBehaviour
         if(newDirection != Direction)
         {
             Direction = newDirection;
+			//playerBody.AddForce(new Vector3(50, 0, 0));
+			//playerAnimator.SetTrigger("got");
+			StartCoroutine(MoveChar(Direction));
             MoveToCube(Direction);
         }
 	}
